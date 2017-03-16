5f4291e72efb543f7b457ad4cd7509d58f746116
Phillip Palatas <PhillipPalatas@Phillips-MacBook-Pro-3.local>
2017-03-16 05:03:15 -0500

    Finished the last two loops

--- foreach.php
+++ foreach.php
@@ -1,4 +1,5 @@
 <?php 
+
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach($things as $thing){
@@ -22,24 +23,24 @@ foreach($things as $thing){
 
 	} else if (is_bool($thing)){
 		echo "{$thing}" . PHP_EOL;
+
 	} else if (is_integer($thing)){
 		echo "{$thing}" . PHP_EOL;
 	} 
 }
 
-// Construct a loop that iterates through each value and outputs its type as either integer, float, boolean, array, null, or string.
+foreach($things as $thing){
+	if (is_scalar($thing)){
+		echo "{$thing}" . " " . "is Scalar" . PHP_EOL;
+	}
+}
 
-// Construct a loop that iterates through each value and outputs only values with a type that is scalar.
 
-// Create a loop that will echo out every value, including those nested in arrays. Output should look like this.
+foreach($things as $element){
 
-//  Sgt. Pepper
-//  11
+	echo (gettype($element)) . PHP_EOL;
+}
 
-//  Array (loop through inner array display here)
-//  3.14
-//  12 + 7
 
-//  11
 
 
