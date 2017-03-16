cf6805db97b7a3980cda5ca31ad5b4859a029611
Phillip Palatas <PhillipPalatas@Phillips-MacBook-Pro-3.local>
2017-03-15 22:48:12 -0500

    Finished the For Exercise

--- for/for.php
+++ for/for.php
@@ -2,27 +2,47 @@
 
 
 // print off even numbers from numbers inputted by a user 
-fwrite(STDOUT, 'Enter in the starting even number');
 
-// input
+
+fwrite(STDOUT, "Enter in the starting even number\n");
+	
 $start = trim(fgets(STDIN));
+	
+fwrite(STDOUT, "Enter the ending number.\n");
 
 $end = trim(fgets(STDIN));
 
-//process modulus
+fwrite(STDOUT, "Enter the number to count by: \n");
+
+$countBy = trim(fgets(STDIN));
+	
+	if (empty($countBy)){
+		$countBy = 1;
+
+	}
+// input
+
+
+	if (!is_numeric($start) || !is_numeric($end)){
+
+		fwrite(STDOUT, "Your input has to be numeric \n");
+	}
 
 /* initialization, */
-for ($i = $start; $i <= $end ; i++){
-	echo $i;
+for ($i = $start; $i <= $end; $i+= $countBy){
+
+	//process modulus
+	// if ($i % 2 === 0){
+
+	//output: only even numbers 
+	echo $i . PHP_EOL;
+
+	// }
 }
 
 //pair of numbers: i.e. min and max which would be an argv value 
 
 //modulus would be used to print the even numbers 
-
-//output: only even numbers 
-
-
 
 
 
