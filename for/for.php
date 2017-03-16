fa3963a0991ef60ac9be538cc7befb8c66268e04
Phillip Palatas <PhillipPalatas@Phillips-MacBook-Pro-3.local>
2017-03-15 21:38:46 -0500

    finishing the highlow file && starting into for exercise

---
+++ for/for.php
@@ -0,0 +1,42 @@
+<?php 
+
+
+// print off even numbers from numbers inputted by a user 
+fwrite(STDOUT, 'Enter in the starting even number');
+
+// input
+$start = trim(fgets(STDIN));
+
+$end = trim(fgets(STDIN));
+
+//process modulus
+
+/* initialization, */
+for ($i = $start; $i <= $end ; i++){
+	echo $i;
+}
+
+//pair of numbers: i.e. min and max which would be an argv value 
+
+//modulus would be used to print the even numbers 
+
+//output: only even numbers 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
